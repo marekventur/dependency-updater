b3186aaec4c62ffd094b274e23a16e640f4b16a0
Marek Ventur <marekventur@gmail.com>
2015-12-05 23:24:42 +0000

    Retry pull requests if github complains about abuse

---
+++ test/github_retry_wrapper.spec.js
@@ -0,0 +1,56 @@
+"use strict";
+
+let retryWrapper = require("../lib/github_retry_wrapper");
+let expect = require("chai").expect;
+let assert = require("chai").assert;
+
+describe("githubRetryWrapper", () => {
+    let abuseError;
+
+    beforeEach(() => {
+        abuseError = new Error("You have triggered an abuse detection mechanism and have been temporarily blocked from content creation. Please retry your request again later.");
+        abuseError.code = 403;
+
+        retryWrapper.RETRY_DELAY = 1;
+        retryWrapper.RETRY_DELAY_MAX = 128;
+    })
+
+    it("passes normal response through", () => {
+        return retryWrapper(() => Promise.resolve("foobar"))
+        .then(response => {
+            expect(response).to.equal("foobar");
+        })
+    });
+
+    it("retries multiple times", () => {
+        let i = 0;
+        function foo() {
+            i++;
+            if (i === 3) {
+                return Promise.resolve("foobar");
+            }
+
+            return Promise.reject(abuseError);
+        }
+
+        return retryWrapper(() => foo())
+        .then(response => {
+            expect(response).to.equal("foobar");
+        })
+    });
+
+    it("passes normal errors through", () => {
+        let error = new Error("other");
+        return retryWrapper(() => Promise.reject(error))
+        .then(() => assert.fail(), err => {
+            expect(err).to.equal(error);
+        })
+    });
+
+    it("times out", () => {
+        return retryWrapper(() => Promise.reject(abuseError))
+        .then(() => assert.fail(), err => {
+            expect(err).to.equal(abuseError);
+        })
+    });
+});
